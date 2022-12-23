9fa45eebd3cabbdc8a2aa27fd1984ec5c493657d
Matthias Gerstner <Matthias.Gerstner@nefkom.net>
2022-12-23 15:46:49 +0100

    types: clearer handling of invalid XID related types

--- include/X++/types.hxx
+++ include/X++/types.hxx
@@ -1,6 +1,9 @@
 #ifndef XPP_TYPES_HXX
 #define XPP_TYPES_HXX
 
+// Cosmos
+#include "cosmos/errors/RuntimeError.hxx"
+
 /* a place for miscellaneous types used in Xpp interfaces */
 
 namespace xpp {
@@ -19,18 +22,32 @@ struct Extent {
 	unsigned int height = 0;
 };
 
+/// constant for an invalid XID value
+/**
+ * Many primitive X types like Window or Pixmap are actually just typedefs for
+ * XID, which in turn is just an unsigned long. I couldn't find official
+ * documentation stating what an invalid XID value might be, but in the
+ * implementation of libX11 I found this here used to flag invalid XID values
+ * so let's do that, too.
+ **/
+constexpr unsigned long INVALID_XID = ~0UL;
+
 struct PixMap {
 	PixMap() {}
-	explicit PixMap(Pixmap pm) : m_valid(true), m_pixmap(pm) {}
+	explicit PixMap(Pixmap pm) : m_pixmap(pm) {}
 	operator Pixmap() const {
 		return id();
 	}
-	Pixmap id() const { return m_pixmap; }
-	void reset() { m_valid = false; }
-	bool valid() const { return m_valid; }
+	Pixmap id() const {
+		if (!valid()) {
+			cosmos_throw( cosmos::RuntimeError("Attempt to use invalid Pixmap ID") );
+		}
+		return m_pixmap;
+	}
+	void reset() { m_pixmap = INVALID_XID; }
+	bool valid() const { return m_pixmap != INVALID_XID; }
 protected:
-	bool m_valid = false;
-	Pixmap m_pixmap = -1;
+	Pixmap m_pixmap = INVALID_XID;
 };
 
 } // end ns
