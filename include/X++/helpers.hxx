62526e160bc9f25f4045f812e205133624b30e91
Matthias Gerstner <Matthias.Gerstner@nefkom.net>
2023-01-06 18:01:14 +0100

    add helper for creating a shared_ptr wrapper around raw libX11 objects
    Add a new header for general purpose helpers for this.

---
+++ include/X++/helpers.hxx
@@ -0,0 +1,25 @@
+#ifndef XPP_HELPERS_HXX
+#define XPP_HELPERS_HXX
+
+// stdlib
+#include <memory>
+#include <exception>
+
+namespace xpp {
+
+/// returns a shared_ptr object that calls XFree on cleanup for XLib memory
+/**
+ * Additionally if a nullptr is passed in (XLib allocation error) then a
+ * std::bad_alloc() exception is thrown.
+ **/
+template <typename T>
+auto make_shared_xptr(T *ptr) {
+	if (!ptr) {
+		throw std::bad_alloc();
+	}
+	return std::shared_ptr<T>(ptr, [](T *dp) { XFree(dp); });
+}
+
+} // end ns
+
+#endif // inc. guard
