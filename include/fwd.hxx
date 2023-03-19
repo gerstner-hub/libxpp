37ab9fee55fd00ddb1c9a9887ba14d87e0e80c85
Matthias Gerstner <Matthias.Gerstner@nefkom.net>
2023-03-19 12:56:40 +0100

    fwd.hxx: add header with helpful forward declarations

---
+++ include/fwd.hxx
@@ -0,0 +1,11 @@
+/**
+ * @file
+ *
+ * This header contains forward declarations of often required types found in
+ * the libX++ library.
+ **/
+
+namespace xpp {
+	class XDisplay;
+	class Pixmap;
+}
