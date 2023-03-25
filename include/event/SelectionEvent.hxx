15b807d0ebaec691f1b360d21522c981327026ef
Matthias Gerstner <Matthias.Gerstner@nefkom.net>
2023-03-25 12:37:50 +0100

    SelectionEvent: add more accessors and also setters

--- include/event/SelectionEvent.hxx
+++ include/event/SelectionEvent.hxx
@@ -10,20 +10,45 @@ namespace xpp {
 class SelectionEvent {
 public: // functions
 
-	explicit SelectionEvent(const Event &ev) :
-		m_ev{ev.toSelectionNotify()} {}
+	explicit SelectionEvent(Event &ev) :
+			m_ev{ev.toSelectionNotify()} {}
 
 	/// Returns the atom corresponding to the property where the selection is stored on
 	AtomID property() const { return AtomID{m_ev.property}; }
 
+	void setProperty(const AtomID property) {
+		m_ev.property = raw_atom(property);
+	}
+
 	/// Returns the target conversion type
 	AtomID target() const { return AtomID{m_ev.target}; }
+
+	void setTarget(const AtomID target) {
+		m_ev.target = raw_atom(target);
+	}
 
 	/// Returns the selection this is about (primary, clipboard, ...)
 	AtomID selection() const { return AtomID{m_ev.selection}; }
 
+	void setSelection(const AtomID id) {
+		m_ev.selection = raw_atom(id);
+	}
+
+	/// Returns the window ID that requested the selection
+	WinID requestor() const { return WinID{m_ev.requestor}; }
+
+	void setRequestor(const WinID id) {
+		m_ev.requestor = raw_win(id);
+	}
+
+	Time time() const { return m_ev.time; }
+
+	void setTime(const Time t) {
+		m_ev.time = t;
+	}
+
 protected: // data
-	const XSelectionEvent &m_ev;
+	XSelectionEvent &m_ev;
 };
 
 } // end ns
