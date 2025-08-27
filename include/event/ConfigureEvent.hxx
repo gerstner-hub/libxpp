7f3dc51f84c48f2a5968c861b9e8303e0ab32b5b
Matthias Gerstner <Matthias.Gerstner@nefkom.net>
2025-08-27 19:45:13 +0200

    ConfigureEvent: allow access to full window coordinates and WinID

--- include/event/ConfigureEvent.hxx
+++ include/event/ConfigureEvent.hxx
@@ -12,12 +12,18 @@ public: // functions
 	explicit ConfigureEvent(const Event &ev) :
 		m_ev{ev.toConfigureNotify()} {}
 
-	/// Returns the inner size of the window not including borders.
-	Extent extent() const {
-		return Extent{
-			 m_ev.width > 0 ? static_cast<unsigned int>(m_ev.width)  : 0,
+	/// Returns the position and inner size of the window not including borders.
+	WindowSpec spec() const {
+		return WindowSpec{
+			m_ev.x,
+			m_ev.y,
+			m_ev.width > 0 ? static_cast<unsigned int>(m_ev.width)  : 0,
 			m_ev.height > 0 ? static_cast<unsigned int>(m_ev.height) : 0
 		};
+	}
+
+	WinID window() const {
+		return WinID{m_ev.window};
 	}
 
 protected: // data
