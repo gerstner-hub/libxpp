d5d297f0f46fefa1c27e4a31b3f00b82525acfc4
Matthias Gerstner <Matthias.Gerstner@nefkom.net>
2025-08-27 19:45:13 +0200

    SelectionEvent: split up into read-only and read-write access types

--- include/event/SelectionEvent.hxx
+++ include/event/SelectionEvent.hxx
@@ -9,38 +9,57 @@ namespace xpp {
 class SelectionEvent {
 public: // functions
 
-	explicit SelectionEvent(Event &ev) :
+	explicit SelectionEvent(const Event &ev) :
 			m_ev{ev.toSelectionNotify()} {}
 
 	/// Returns the atom corresponding to the property where the selection is stored on
 	AtomID property() const { return AtomID{m_ev.property}; }
 
+	/// Returns the target conversion type
+	AtomID target() const { return AtomID{m_ev.target}; }
+
+	/// Returns the selection this is about (primary, clipboard, ...)
+	AtomID selection() const { return AtomID{m_ev.selection}; }
+
+	/// Returns the window ID that requested the selection
+	WinID requestor() const { return WinID{m_ev.requestor}; }
+
+	Time time() const { return m_ev.time; }
+
+protected: // functions
+
+	explicit SelectionEvent(const XSelectionEvent &ev) :
+		m_ev{ev} {}
+
+protected: // data
+	const XSelectionEvent &m_ev;
+};
+
+/// Wrapper around the XSelectionEvent type with data modification support.
+class SelectionEventBuilder :
+		public SelectionEvent {
+public: // functions
+
+	SelectionEventBuilder(Event &ev) :
+		SelectionEvent{ev.toSelectionNotify()},
+		m_ev{ev.toSelectionNotify()} {
+	}
+
 	void setProperty(const AtomID property) {
 		m_ev.property = raw_atom(property);
 	}
-
-	/// Returns the target conversion type
-	AtomID target() const { return AtomID{m_ev.target}; }
 
 	void setTarget(const AtomID target) {
 		m_ev.target = raw_atom(target);
 	}
 
-	/// Returns the selection this is about (primary, clipboard, ...)
-	AtomID selection() const { return AtomID{m_ev.selection}; }
-
 	void setSelection(const AtomID id) {
 		m_ev.selection = raw_atom(id);
 	}
 
-	/// Returns the window ID that requested the selection
-	WinID requestor() const { return WinID{m_ev.requestor}; }
-
 	void setRequestor(const WinID id) {
 		m_ev.requestor = raw_win(id);
 	}
-
-	Time time() const { return m_ev.time; }
 
 	void setTime(const Time t) {
 		m_ev.time = t;
